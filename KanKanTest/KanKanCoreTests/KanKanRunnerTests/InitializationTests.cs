c94f32a143ea222dec71bd6d78391b2b4edf38fa
Robin Lacey <robin@madetech.com>
2019-01-28 18:24:53 +0000

    Added test to check for correct Current value with IEnumerator

--- KanKanTest/KanKanCoreTests/KanKanRunnerTests/InitializationTests.cs
+++ KanKanTest/KanKanCoreTests/KanKanRunnerTests/InitializationTests.cs
@@ -31,5 +31,21 @@ namespace KanKanTest.KanKanCoreTests.KanKanRunnerTests
             IKanKanRunner<IKanKan> kanKanRunner = new KanKanSingleRunner(kanKanDummy, string.Empty);
             Assert.AreSame(kanKanRunner.Current, kanKanDummy);
         }
+        
+        [Test]
+        public void IEnumeratorCurrentIsCorrectlySet()
+        {
+            IKanKan kanKanDummy = new KanKanDummy();
+            IKanKanRunner<IKanKan> kanKanRunner = new KanKanSingleRunner(kanKanDummy, string.Empty);
+            IEnumerator singleAsIEnumerator = kanKanRunner;
+            Assert.AreSame((IKanKan) singleAsIEnumerator.Current,kanKanDummy);
+
+
+            IKanKan[] kanKansDummy = {new KanKanDummy(), new KanKanDummy()};
+            IKanKanRunner<IKanKan[]> kanKansRunner = new KanKanSequenceRunner(kanKansDummy, string.Empty);
+            IEnumerator sequenceAsIEnumerator = kanKansRunner;
+            Assert.AreSame((IKanKan) sequenceAsIEnumerator.Current,kanKansDummy[0]);
+            
+        }
     }
 }
